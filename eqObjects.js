88f72066fb79f92c1c65fe96d5e8053cc6c4eb87
Emily Lim <emily.elim04@gmail.com>
2021-01-07 01:00:45 +0000

    creating eqObjects function

---
+++ eqObjects.js
@@ -0,0 +1,72 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//eqArrays function for checking the inside an array if the values are equal
+const eqArrays = function(arrayOne, arrayTwo) {
+  
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+
+  return true;
+
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+
+  let objOneKeys = Object.keys(object1);
+  let objTwoKeys = Object.keys(object2);
+  
+  if (objOneKeys.length !== objTwoKeys.length) {
+    return false;
+  }
+
+  for (let key of objOneKeys) {
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+
+};
+
+
+//TEST CODE
+
+//for string based values in objects
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
+
+const el = { a: "1", b: "2" };
+const jk = { b: "2", a: "4" };
+assertEqual(eqObjects(el, jk), false); // => true
+
+//for arrays in objects as values
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
