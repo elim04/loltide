34d8289130b95b6f9dded624e6d4a6d012c0dc22
Emily Lim <emily.elim04@gmail.com>
2021-01-11 17:42:11 +0000

    refactoring assertEqual function to include module.exports and be used in other functions

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("cookies", "cookies");
-assertEqual(4, 5);
+module.exports = assertEqual;
